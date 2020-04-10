d3dc248517e4e129ecde8812e6ad9caf5e99eff2
Trisert <nicolade03@gmail.com>
2020-04-10 12:03:00 +0200

    script

---
+++ setup.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+
+pacman -S git zsh firefox
+
+useradd -m -G wheel -s /bin/zsh nicola
+
+passwd nicola
+
+su - nicola
+
+git clone https://aur.archlinux.org/yay.git
+
+cd yay
+
+makepkg -sic
+
+yay -S oh-my-zsh-git vim
+
+mkdir .config/i3
+
+wget -P .config/i3 https://raw.githubusercontent.com/Trisert/dotfiles/master/.config/i3/config
+
+mkdir st
+
+cd st
+
+wget https://raw.githubusercontent.com/Trisert/st/master/PKGBUILD
+
+makepkg -sic
+
+cd ..
+
+cd dmenu
+
+wget https://raw.githubusercontent.com/Trisert/dmenu/master/PKGBUILD
+
+makepkg -sic
+
+cd .. 
+
+wget https://raw.githubusercontent.com/Trisert/dotfiles/master/.zshrc
+
+wget https://raw.githubusercontent.com/Trisert/dotfiles/master/.vimrc
+
+wget https://raw.githubusercontent.com/Trisert/dotfiles/master/.xinitrc
